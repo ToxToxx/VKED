9f8dae2c9110590100686ea57271f1008ae5c723
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2024-01-14 16:42:05 +0300

    задание на поиск треугольника

---
+++ Graphs/trianglezad.py
@@ -0,0 +1,24 @@
+def has_triangle(graph):
+    for node in graph:
+        neighbors = graph[node]
+        for neighbor1 in neighbors:
+            for neighbor2 in neighbors:
+                if neighbor1 != neighbor2 and neighbor2 in graph[neighbor1]:
+                    return "YES"
+    return "NO"
+
+n = int(input())
+edges = [tuple(map(int, input().split())) for _ in range(n)]
+
+graph = {}
+for edge in edges:
+    a, b = edge
+    if a not in graph:
+        graph[a] = set()
+    if b not in graph:
+        graph[b] = set()
+    graph[a].add(b)
+    graph[b].add(a)
+
+result = has_triangle(graph)
+print(result)
