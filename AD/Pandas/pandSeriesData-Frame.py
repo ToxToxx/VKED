4a99aeee5955fd8d88c8b1d36f10f618d6a3aed9
Matvey <sinema3d00@gmail.com>
2024-01-26 23:00:12 +0300

    изучаю пандас

--- AD/Pandas/pandSeriesData-Frame.py
+++ AD/Pandas/pandSeriesData-Frame.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+
+
 #Series - объект похожий на одномерный массив
+my_series = pd.Series([5,4,6,8,10,20])
+print(my_series)
+print(my_series.index)
+print(my_series.values)
