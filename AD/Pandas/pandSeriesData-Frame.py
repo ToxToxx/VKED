2fbf0cbba6e97333180922823df04d5157cd1c8b
Matvey <sinema3d00@gmail.com>
2024-01-27 16:09:51 +0300

    Update pandSeriesData-Frame.py

--- AD/Pandas/pandSeriesData-Frame.py
+++ AD/Pandas/pandSeriesData-Frame.py
@@ -14,8 +14,37 @@ my_series2 = pd.Series([1, 2, 3, 4, 5,],
 print("второй массив\n", my_series2)
 print(my_series[2])
 print(my_series2["d"])
+
 mask = my_series > 6
 print("Маска \n", mask)
 print(my_series[mask])
+
 my_series2[ ["a", "b", "c"] ] = 5
 print("измененный\n", my_series2)
+print(my_series[my_series < 8])
+
+my_series3 = pd.Series({ "a" : 1, "b" : 2, "c" : 3, "d" : 4, "g" : 5})
+print("Третий\n", my_series3)
+my_series3.name = "bukvi"
+my_series3.index.name = "privet"
+print("Изменение\n", my_series3)
+
+#pandas DataFrame = таблица
+print("             DATAFRAME           ")
+df = pd.DataFrame({
+    'name': ['Alexey', 'Maxim', 'Sergey'],
+    'height': [188, 170, 177],
+    'weight': [85, 68, 77]
+})
+print(df)
+print(df["height"])
+print(df[['name']])
+print(df.weight)
+df.index = ["Lexa", "Max", "Serg"]
+print(df)
+
+#loc и iloc
+print(df.loc["Max"])
+print(df.iloc[2])
+print(df.loc[["Max", "Lexa"],"name"])
+print(df.iloc[[0,1], 2])
