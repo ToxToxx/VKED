0eddc4de95f5ea74cf1b27dff5b7deb5ad82d08a
Matvey <sinema3d00@gmail.com>
2024-01-29 19:22:01 +0300

    учусь работать с реальной таблицей

---
+++ AD/Pandas/pandasWorkWithTable.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("E:/Programming/VKED/AD/Pandas/orders.csv")
+
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.precision', 3)
+
+
+print('Tail\n', df.tail())
+print('Head\n', df.head())
+
+print('Shape ', df.shape)
+print('Columns', df.columns)
+print('Describe\n', df.describe())
+print('Describe object\n', df.describe(include=['object']))
