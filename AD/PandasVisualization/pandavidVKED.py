23fd38e182ce9d4a86cb29f205b565776da7fcde
Matvey <sinema3d00@gmail.com>
2024-01-30 19:29:01 +0300

    Update pandavidVKED.py

--- AD/PandasVisualization/pandavidVKED.py
+++ AD/PandasVisualization/pandavidVKED.py
@@ -14,3 +14,25 @@ df['FastingBS'] = np.where(df['FastingBS'] == 0, np.nan, df['FastingBS'])
 fig = plt.figure(figsize = (14, 5))
 df['ChestPainType'].value_counts().plot.bar()
 plt.show()
+
+fig = plt.figure(figsize= (14, 5))
+df['ChestPainType'].value_counts().sort_index().plot.bar()
+plt.show()
+
+fig = plt.figure(figsize= (14, 5))
+(df['ChestPainType'].value_counts() / len(df)).plot.bar()
+plt.show()
+
+print(df.groupby(['HeartDisease'])[['Age', 'Cholesterol']].mean())
+
+df.groupby(['HeartDisease'])[['Age', 'Cholesterol']].mean().plot.bar(figsize = (14,5), stacked = False)
+plt.show()
+
+df.groupby(['HeartDisease'])[['Age', 'Cholesterol']].mean().plot.bar(figsize = (14,5), stacked = True)
+plt.show()
+
+df['Cholesterol_bin'] = df['Cholesterol'] // 10 * 10
+
+fig = plt.figure(figsize= (14,5))
+chart = df['Cholesterol_bin'].value_counts().sort_index().plot.line()
+plt.show()
