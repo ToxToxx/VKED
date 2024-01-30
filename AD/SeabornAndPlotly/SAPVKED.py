383ad3bfe10b6d54b44954f773407b7f5f5e2927
Matvey <sinema3d00@gmail.com>
2024-01-30 22:34:02 +0300

    Update SAPVKED.py

--- AD/SeabornAndPlotly/SAPVKED.py
+++ AD/SeabornAndPlotly/SAPVKED.py
@@ -4,3 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("E:/Programming/VKED/AD/SeabornAndPlotly/heart.csv")
+df['Cholesterol'] = np.where(df['Cholesterol'] == 0, np.nan, df['Cholesterol'])
+df['RestingBP'] = np.where(df['RestingBP'] == 0, np.nan, df['RestingBP'])
+df['FastingBS'] = np.where(df['FastingBS'] == 0, np.nan, df['FastingBS'])
+
+fig = plt.figure(figsize= (14, 5))
+#pandas bar = seaborn countplot
+sns.countplot(x = df['ChestPainType'])
+plt.show()
+
+fig = plt.figure(figuresize = (14,5))
+ax1 = fig.add_subplot(221)
+sns.kdeplot(df.Age.dropna())
+ax2 = fig.add_subplot(222)
+df['Age'].value_counts().sort_index().plot.line()
+plt.show()
