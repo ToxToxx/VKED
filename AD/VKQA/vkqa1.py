18e2e0f7857e3d88a50e2c599120fb08b1422c2d
Matvey <sinema3d00@gmail.com>
2024-02-02 16:57:53 +0300

    Update vkqa1.py

--- AD/VKQA/vkqa1.py
+++ AD/VKQA/vkqa1.py
@@ -30,3 +30,27 @@ sum_of_all_currencies_bigger1 = sum(users_currencies_count['currency'] > 1)
 unique_users = incomes['user_id'].nunique()
 answer3 = round((sum_of_all_currencies_bigger1 / unique_users * 100), 1)
 print('Third ', answer3)
+
+#по каждому клиенту вывести дату первого пополнения
+#валюту,сумму в рублях(использовать вторую таблицу)
+#Ответом вывести общую сумму в рублях по всем клиентам
+
+#1 -найти для каждого клиента запись с самым первым пополнением
+users_first_income = incomes.groupby('user_id')['operation_date'].min().reset_index()
+merging_incomes_firstIncome = pd.merge(incomes, users_first_income, 'inner', ['user_id', 'operation_date'])
+
+#2 - джоин таблиц с пополнениями и с курсами валют
+currencies.loc[currencies['rate_currency'] == 'CNY', 'rate_value'] = currencies.loc[currencies['rate_currency'] == 'CNY', 'rate_value'].apply(lambda x: str(x)[5:7] + '.' + str(x)[:4])
+currencies['rate_value'] = currencies['rate_value'].astype(float)
+
+#3 - нужно вывести сумму в рублях
+first_incomes_with_rate_values = pd.merge(merging_incomes_firstIncome, 
+         currencies, 
+         how = 'left', 
+         left_on = ['currency', 'operation_day'], 
+         right_on = ['rate_currency', 'rate_date'])
+
+first_incomes_with_rate_values['rate_value'] = first_incomes_with_rate_values['rate_value'].fillna(1.0)
+answer4 = sum(first_incomes_with_rate_values['volume'] * first_incomes_with_rate_values['rate_value'])
+
+print('Fourth', answer4)
