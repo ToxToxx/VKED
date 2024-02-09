5554ffe9272761c6be07dca21cfd959f4ad9e8d5
Matvey <sinema3d00@gmail.com>
2024-02-09 16:30:25 +0300

    Update statKritANOVAVKED.py

--- AD/StatKriterii/statKritANOVAVKED.py
+++ AD/StatKriterii/statKritANOVAVKED.py
@@ -13,3 +13,11 @@ fig = plt.figure(figsize= (14,7))
 ax1 = plt.subplot(111)
 plt.hist(gen_pop, 100)
 plt.show()
+
+#извлекаем 3 случ совокупности по 50 заказов в каждой
+np.random.seed(3106)
+sample_groups = []
+for i in range(3):
+    sample_groups.append(np.random.choice(gen_pop, size = 50).astype(int))
+sample_groups = np.array(sample_groups)
+print(sample_groups)
