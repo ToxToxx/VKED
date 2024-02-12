7e4faa659571e36c3dfec89853c32c7be9b2099d
Matvey <sinema3d00@gmail.com>
2024-02-12 19:47:16 +0300

    Update ProverkaHipotezParamKriter.py

--- AD/PipelineProverkiStatHipotez/ProverkaHipotezParamKriteriev/ProverkaHipotezParamKriter.py
+++ AD/PipelineProverkiStatHipotez/ProverkaHipotezParamKriteriev/ProverkaHipotezParamKriter.py
@@ -25,7 +25,7 @@ sample_1 = d.rvs(size=300)#логнормальное
 d = stats.norm(loc = 25, scale = 10)
 sample_2 = d.rvs(size=300)#нормальное 
 
-fig = plt.figure(figsize= (17,10))
+fig = plt.figure(figsize= (14,8))
 ax1 = fig.add_subplot(221)
 plt.hist(sample_1, bins = 20)
 ax2 = fig.add_subplot(222) 
@@ -34,4 +34,35 @@ ax3 = fig.add_subplot(223)
 stats.probplot(sample_1, dist = "norm", plot = plt)
 ax3 = fig.add_subplot(224)
 stats.probplot(sample_2, dist = "norm", plot = plt)
+plt.show()
+
+#КРИТЕРИЙ ШАПИРО УИЛКО применяется до 50 наблюдений
+d = stats.lognorm(0.5, loc = 25, scale = 10)
+sample_1 = d.rvs(size=35)
+
+d = stats.norm(loc = 25, scale = 10)
+sample_2 = d.rvs(size=30)
+
+print(stats.shapiro(sample_1))#отвергается
+print(stats.shapiro(sample_2))#принимается гипотеза ибо при заданном значении p > 0.05
+
+
+#РЕШЕНИЕ ЗАДАЧИ ПРО КОМПЬЮТЕРЫ. ИСПОЛЬЗУЕТСЯ КРИТЕРИЙ СТЬЮДЕНТА
+sample_mean = 102
+mu = 90
+n = 20
+sample_std = 15
+
+t = (sample_mean - mu) / (sample_std/np.sqrt(n))
+print('t ', t)
+
+t_crit = stats.t.ppf(1-0.05, n)
+print('t критическое ', t_crit)
+
+fig = plt.figure(figsize= (10,4))
+xs = np.linspace(-5,5,1000)
+plt.plot(xs, stats.t.pdf(xs, n - 1))
+plt.axvline(t_crit, color='red', linestyle='dashed', linewidth=3, label = 't-крит')
+plt.axvline(t, color='blue', linestyle='dashed', linewidth=1,label = 't-наблюдаемое')
+plt.legend()
 plt.show()
